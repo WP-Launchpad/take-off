e9d544f4eaa47ff100ea696046799d05a8060beb
Cyrille C <18537428+CrochetFeve0251@users.noreply.github.com>
2023-03-04 21:57:09 +0100

    Added some logic for the service provider

--- inc/ServiceProvider.php
+++ inc/ServiceProvider.php
@@ -6,9 +6,15 @@ use League\Flysystem\Adapter\Local;
 use League\Flysystem\Filesystem;
 use RocketLauncherBuilder\App;
 use RocketLauncherBuilder\Entities\Configurations;
+use RocketLauncherBuilder\ServiceProviders\ServiceProviderInterface;
 use RocketLauncherBuilder\Templating\Renderer;
+use RocketLauncherTakeOff\Commands\InitializeProjectCommand;
+use RocketLauncherTakeOff\Services\NamespaceManager;
+use RocketLauncherTakeOff\Services\PluginFileManager;
+use RocketLauncherTakeOff\Services\PrefixManager;
+use RocketLauncherTakeOff\Services\ProjectManager;
 
-class ServiceProvider implements \RocketLauncherBuilder\ServiceProviders\ServiceProviderInterface
+class ServiceProvider implements ServiceProviderInterface
 {
     /**
      * Interacts with the filesystem.
@@ -23,6 +29,12 @@ class ServiceProvider implements \RocketLauncherBuilder\ServiceProviders\Service
      * @var Configurations
      */
     protected $configs;
+
+    /**
+     * @var Renderer
+     */
+    protected $renderer;
+
     /**
      * Instantiate the class.
      *
@@ -34,6 +46,16 @@ class ServiceProvider implements \RocketLauncherBuilder\ServiceProviders\Service
     {
         $this->configs = $configs;
         $this->filesystem = $filesystem;
+
+        $adapter = new Local(
+        // Determine root directory
+            __DIR__ . '/../'
+        );
+
+        // The FilesystemOperator
+        $template_filesystem = new Filesystem($adapter);
+
+        $this->renderer = new Renderer($template_filesystem, '/templates/');
     }
 
 
@@ -42,7 +64,11 @@ class ServiceProvider implements \RocketLauncherBuilder\ServiceProviders\Service
      */
     public function attach_commands(App $app): App
     {
-
+        $namespace_manager = new NamespaceManager($this->filesystem);
+        $plugin_file_manager = new PluginFileManager($this->filesystem, $this->renderer);
+        $prefix_manager = new PrefixManager($this->filesystem);
+        $project_manager = new ProjectManager($this->filesystem);
+        $app->add(new InitializeProjectCommand($namespace_manager, $plugin_file_manager, $prefix_manager, $project_manager));
         return $app;
     }
 }
